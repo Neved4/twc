eff622fd64d837e088e6beb8095670dec3a48cde
Neveda <63655535+Neved4@users.noreply.github.com>
2023-12-10 00:11:39 +0100

    Simplify code in `twc.c`

--- twc.c
+++ twc.c
@@ -1,11 +1,10 @@
 #define _POSIX_C_SOURCE 200809L
-#define TIMEFMT_DEFAULT TIMEFMT_ISO
 #define STR_LENGTH 32
 
-#include <libgen.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <libgen.h>
 #include <time.h>
 #include <unistd.h>
 
@@ -25,76 +24,49 @@ static void usage(const char *s) {
 	exit(EXIT_FAILURE);
 }
 
-static void err_env(void) {
-	fprintf(stderr, "error: could not get the value of the $HOME variable");
-	exit(EXIT_FAILURE);
-}
-
-static void err_file(const char *s) {
-	fprintf(stderr, "error: configuration file not found in %s\n", s);
+static void errmsg(const char *msg) {
+	fprintf(stderr, "error: %s\n", msg);
 	exit(EXIT_FAILURE);
 }
 
 static void fparse(const char *s) {
-	int hc = 0;
 	char timestr[STR_LENGTH];
-	char *nl = NULL;
-	char *sep = NULL;
-	char *line = NULL;
-	char *path = NULL;
-	char *conf = "/.config";
-	char *home = getenv("XDG_CONFIG_HOME");
-	char *file = "/twc/tz.conf";
+	char *line = NULL, *path = NULL, *sep = NULL;
+	size_t sizet;
 	FILE *fp;
 
-	if (home == NULL || *home == '\0') {
-		hc = 1;
-		home = getenv("HOME");
+	char *home = getenv("XDG_CONFIG_HOME");
+	home = (!home || !*home) ? getenv("HOME") : home;
+	if (!home || !*home)
+		errmsg("could not get the value of the $HOME variable");
 
-		if (home == NULL || *home == '\0') {
-			err_env();
-		}
-	}
+	char *conf = home == getenv("HOME") ? "/.config" : "";
+	char *file = "/twc/tz.conf";
+	sizet = strlen(home) + strlen(conf) + strlen(file) + 1;
+	path = malloc(sizet);
+	snprintf(path, sizet, "%s%s%s", home, conf, file);
 
-	size_t sizet = strlen(home) + strlen(file);
-	path = (char *) malloc(sizet);
-	strcpy(path, home);
-
-	if (hc == 1) {
-		sizet += strlen(conf);
-		path = (char *) realloc(path, sizet);
-		strcat(path, conf);
-	}
-
-	strcat(path, file);
 	fp = fopen(path, "r");
-
-	if (fp == NULL) {
-		err_file(path);
-	}
+	if (!fp)
+		errmsg(strcat("configuration file not found in ", path));
 
 	size_t len = 0;
-	ssize_t(read);
+	ssize_t read;
 	time_t t;
 	time(&t);
 
 	while ((read = getline(&line, &len, fp)) != -1) {
-		if (*line == '#' || *line == '\n') {
+		if (*line == '#' || *line == '\n')
 			continue;
-		}
 
-		nl = strchr(line, '\n');
-
-		if (nl) {
-			*nl = '\0';
-		}
+		char *nl = strchr(line, '\n');
+		if (nl) *nl = '\0';
 
 		setenv("TZ", line, 1);
 		strftime(timestr, sizeof(timestr), s, localtime(&t));
 
-		while ((sep = strchr(line, '_'))) {
+		while ((sep = strchr(line, '_')))
 			*sep = ' ';
-		}
 
 		printf("%-24s%s\n", line, timestr);
 	}
@@ -105,9 +77,9 @@ static void fparse(const char *s) {
 }
 
 int main(int argc, char **argv) {
-	int opt;
 	const char *progname = basename(argv[0]);
-	const char *timefmt = time_fmts[TIMEFMT_DEFAULT];
+	const char *timefmt = time_fmts[TIMEFMT_ISO];
+	int opt;
 	opterr = 0;
 
 	while ((opt = getopt(argc, argv, "h")) != -1) {
@@ -120,10 +92,10 @@ int main(int argc, char **argv) {
 				break;
 			default:
 				usage(progname);
-				return(1);
+				return 1;
 		}
 	}
 
 	fparse(timefmt);
-	return(0);
+	return 0;
 }
