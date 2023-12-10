e6e3e8567955a43cb7d2bbd1c118f1d56f393fe9
Neveda <63655535+Neved4@users.noreply.github.com>
2023-12-10 01:43:47 +0100

    Simplify `getopt()` code

--- twc.c
+++ twc.c
@@ -20,7 +20,7 @@ const char *time_fmts[NTIMEFMT] = {
 };
 
 static void usage(const char *s) {
-	fprintf(stderr, "usage: %s [-h] ...\n", s);
+	fprintf(stderr, "usage: %s [-h] [-f FILE]\n", s);
 	exit(EXIT_FAILURE);
 }
 
@@ -29,38 +29,41 @@ static void errmsg(const char *msg) {
 	exit(EXIT_FAILURE);
 }
 
-static void fparse(const char *s) {
+static void fparse(const char *s, const char *fpath) {
 	char timestr[STR_LENGTH];
 	char *line = NULL, *path = NULL, *sep = NULL;
-	size_t sizet;
+	size_t len = 0;
 	FILE *fp;
 
-	char *home = getenv("XDG_CONFIG_HOME");
-	home = (!home || !*home) ? getenv("HOME") : home;
-	if (!home || !*home)
-		errmsg("could not get the value of the $HOME variable");
+	if (!fpath) {
+		char *home = getenv("XDG_CONFIG_HOME");
+		home = (!home || !*home) ? getenv("HOME") : home;
+		if (!home || !*home)
+			errmsg("could not get the value of the $HOME variable");
 
-	char *conf = home == getenv("HOME") ? "/.config" : "";
-	char *file = "/twc/tz.conf";
-	sizet = strlen(home) + strlen(conf) + strlen(file) + 1;
-	path = malloc(sizet);
-	snprintf(path, sizet, "%s%s%s", home, conf, file);
+		char *conf = home == getenv("HOME") ? "/.config" : "";
+		char *file = "/twc/tz.conf";
+		size_t sizet = strlen(home) + strlen(conf) + strlen(file) + 1;
+		char *path = malloc(sizet);
+		snprintf(path, sizet, "%s%s%s", home, conf, file);
 
-	fp = fopen(path, "r");
+		fpath = path;
+	}
+
+	fp = fopen(fpath, "r");
 	if (!fp)
-		errmsg(strcat("configuration file not found in ", path));
+		errmsg(strcat("configuration file not found in ", fpath));
 
-	size_t len = 0;
-	ssize_t read;
 	time_t t;
 	time(&t);
 
-	while ((read = getline(&line, &len, fp)) != -1) {
+	while (getline(&line, &len, fp) != -1) {
 		if (*line == '#' || *line == '\n')
 			continue;
 
 		char *nl = strchr(line, '\n');
-		if (nl) *nl = '\0';
+		if (nl)
+			*nl = '\0';
 
 		setenv("TZ", line, 1);
 		strftime(timestr, sizeof(timestr), s, localtime(&t));
@@ -77,18 +80,18 @@ static void fparse(const char *s) {
 }
 
 int main(int argc, char **argv) {
+	int opt;
 	const char *progname = basename(argv[0]);
 	const char *timefmt = time_fmts[TIMEFMT_ISO];
-	int opt;
-	opterr = 0;
+	const char *fpath = NULL;
 
-	while ((opt = getopt(argc, argv, "h")) != -1) {
+	while ((opt = getopt(argc, argv, "hf:")) != -1) {
 		switch (opt) {
 			case 'h':
 				timefmt = time_fmts[TIMEFMT_HUMAN];
 				break;
-			case '?':
-				fprintf(stderr, "%s: invalid option -%c\n", progname, optopt);
+			case 'f':
+				fpath = optarg;
 				break;
 			default:
 				usage(progname);
@@ -96,6 +99,7 @@ int main(int argc, char **argv) {
 		}
 	}
 
-	fparse(timefmt);
+	fparse(timefmt, fpath);
+
 	return 0;
 }
